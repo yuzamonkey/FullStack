8df701333525ad2c2a82cfbaccfa10554a65af45
Jusa Mankki <jusamankki@gmail.com>
2021-01-12 17:18:23 +0200

    1.5 tehty

--- viikko1/kurssitiedot/src/index.js
+++ viikko1/kurssitiedot/src/index.js
@@ -27,34 +27,36 @@ const Part = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.parts[0].exercises 
-      + props.parts[1].exercises 
-      + props.parts[2].exercises}</p> 
+    <p>Number of exercises {props.parts[0].exercises
+      + props.parts[1].exercises
+      + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header text={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div >
   )
 }
