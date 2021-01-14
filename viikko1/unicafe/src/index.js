18f3718edcbf3e3eff68ccd4559d1a5b425be44a
Jusa Mankki <jusamankki@gmail.com>
2021-01-14 09:48:52 +0200

    unicafe 1.6

---
+++ viikko1/unicafe/src/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Title = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Title title={'Give feedback'} />
+      <Button handleClick={increaseGood} text={'good'} />
+      <Button handleClick={increaseNeutral} text={'neutral'} />
+      <Button handleClick={increaseBad} text={'bad'} />
+      <Title title={'Statistics'} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
