cd41b777cd9965247fd6c36d33c0459418c55fd3
Jusa Mankki <jusamankki@gmail.com>
2021-01-14 15:28:57 +0200

    1.7 ja 1.8

--- viikko1/unicafe/src/index.js
+++ viikko1/unicafe/src/index.js
@@ -15,24 +15,63 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, all, average, percentage }) => {
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>percentage {percentage} %</p>
     </div>
   )
 }
+
+const CountAverage = (array) => {
+  let sum = 0
+  for (let value of array) {
+    sum = sum + value
+  }
+  return sum / array.length
+}
+
+
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
+  const [average, setAverage] = useState(0)
+  const [percentage, setPercentage] = useState(0)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+
+
+  const increaseGood = () => {
+    const newGood = good+1
+    const newAll = all.concat(1)
+    setAverage(CountAverage(newAll))
+    setPercentage(newGood/newAll.length)
+    setGood(good + 1)
+    setAll(newAll)
+  }
+  const increaseNeutral = () => {
+    const newNeutral = neutral+1
+    const newAll = all.concat(0)
+    setAverage(CountAverage(newAll))
+    setPercentage(good/newAll.length)
+    setNeutral(newNeutral)
+    setAll(newAll)
+  }
+  const increaseBad = () => {
+    const newBad = bad + 1
+    const newAll = all.concat(-1)
+    setAverage(CountAverage(newAll))
+    setPercentage(good/newAll.length)
+    setBad(newBad)
+    setAll(newAll)
+  }
 
   return (
     <div>
@@ -41,7 +80,13 @@ const App = () => {
       <Button handleClick={increaseNeutral} text={'neutral'} />
       <Button handleClick={increaseBad} text={'bad'} />
       <Title title={'Statistics'} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all.length}
+        average={average}
+        percentage={percentage} />
     </div>
   )
 }
