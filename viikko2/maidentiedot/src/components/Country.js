2b1656e687680f9064cba526823bdc07109f6180
Jusa Mankki <jusamankki@gmail.com>
2021-01-22 22:03:36 +0200

    2.14

--- viikko2/maidentiedot/src/components/Country.js
+++ viikko2/maidentiedot/src/components/Country.js
@@ -2,19 +2,17 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export const Country = ({ country }) => {
-  const [weatherData, setWeatherData] = useState({})
+  const [weatherData, setWeatherData] = useState(undefined)
+
   const api_key = process.env.REACT_APP_API_KEY
-  const url = ''
-  const hook = () => {
+  const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+  useEffect(() => {
     axios
       .get(url)
       .then(response => {
-        console.log('weather promise fulfilled', response.data)
         setWeatherData(response.data)
       })
-  }
-  useEffect(hook, [])
-  console.log('weatherData', weatherData)
+  }, [])
 
   return (
     <div>
@@ -26,10 +24,14 @@ export const Country = ({ country }) => {
         {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
       <img src={country.flag} width="200" alt="flag" />
-      {/* <h3>Weather in {country.capital}</h3>
-      <p><b>temperature: </b>{weatherData.current.temperature} Celcius</p>
-      <img src={weatherData.current.weather_icons[0]} alt="weatherpic" />
-      <p><b>wind: </b>{weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p> */}
+      {(weatherData !== undefined) &&
+        <div>
+          <h3>Weather in {country.capital}</h3>
+          <p><b>temperature: </b>{weatherData.current.temperature} Celcius</p>
+          <img src={weatherData.current.weather_icons[0]} alt="weatherpic" />
+          <p><b>wind: </b>{weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p>
+        </div>
+      }
     </div>
   )
 }
