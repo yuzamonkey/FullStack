b0b651e12485c5d3cd2debf06b9bd254ac9171eb
Jusa Mankki <jusamankki@gmail.com>
2021-01-26 11:50:49 +0200

    3.8

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-app.use(morgan('tiny'))
+morgan.token('data', function(req, res) {return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
+
 
 
 let persons = [
