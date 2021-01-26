9c70ccaad77ee620048f5d15f4a885cd063d1925
Jusa Mankki <jusamankki@gmail.com>
2021-01-26 09:44:17 +0200

    3.2

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const firstLine = `Phonebook has info for ${persons.length} people`
+  const time = new Date()
+  const content = `<p>${firstLine}</p> <p>${time}</p>`
+  response.send(content)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
