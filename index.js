c46a63d3d506d0cf095a3b7c9d10b999b1765c48
Jusa Mankki <jusamankki@gmail.com>
2021-01-29 14:25:10 +0200

    3.16

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
+app.use(express.json())
+//app.use(logger)
 const Person = require('./models/person')
 const morgan = require('morgan')
 morgan.token('data', function(req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-app.use(express.json())
 const cors = require('cors')
 app.use(cors())
-app.use(express.static('build'))
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
 })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 app.get('/info', (request, response) => {
   const firstLine = `Phonebook has info for ${persons.length} people`
@@ -23,10 +37,15 @@ app.get('/info', (request, response) => {
   response.send(content)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -40,15 +59,12 @@ app.post('/api/persons', (request, response) => {
   })
   person.save().then(savedPerson => {
     response.json(savedPerson)
-    //console.log("SAVED PERSON", savedPerson)
   })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  console.log("APP.DELETE CALLED")
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
-      console.log("DELETE RESULT", result)
       response.status(204).end()
     })
     .catch(error => next(error))
