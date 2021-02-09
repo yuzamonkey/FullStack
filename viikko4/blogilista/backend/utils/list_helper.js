4e46948a1f0a6cc8f9ce6c12acc1677a4646aaff
Jusa Mankki <jusamankki@gmail.com>
2021-02-09 11:13:52 +0200

    4.4

--- viikko4/blogilista/backend/utils/list_helper.js
+++ viikko4/blogilista/backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return blogs.length === 0 ? 0 : likes.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
