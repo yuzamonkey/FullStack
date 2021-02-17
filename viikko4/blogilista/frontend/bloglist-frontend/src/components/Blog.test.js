2a2b2b2a5b3d93cb940738d73baea57521950fd2
Jusa Mankki <jusamankki@gmail.com>
2021-02-17 15:21:47 +0200

    5.14

--- viikko4/blogilista/frontend/bloglist-frontend/src/components/Blog.test.js
+++ viikko4/blogilista/frontend/bloglist-frontend/src/components/Blog.test.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author but not url and likes', () => {
-    const blog = {
-        title: 'test title',
-        author: 'test author',
-        url: 'test url',
-        likes: 123,
-        user: {
-            name: 'test user.name'
-        }
+const blog = {
+    title: 'test title',
+    author: 'test author',
+    url: 'test url',
+    likes: 123,
+    user: {
+        name: 'test user.name'
     }
+}
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+test('renders title and author but not url and likes', () => {
+    const component = render(
+        <Blog blog={blog} />
+    )
+    expect(component.container).toHaveTextContent(
+        'test title', 'test view'
+    )
+    expect(component.container).not.toHaveTextContent(
+        'test url', 'likes'
+    )
+})
 
-  expect(component.container).toHaveTextContent(
-    'test title', 'test view'
-  )
-  expect(component.container).not.toHaveTextContent(
-    'test url', 'likes'
-  )
+test('clicking the view button shows url and likes', () => {
+    const component = render (
+        <Blog blog={blog} />
+    )
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent(
+        'test url', 'likes'
+    )
+
 })
