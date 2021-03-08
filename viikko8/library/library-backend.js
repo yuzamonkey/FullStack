055718b9edce4a3044c795a8b46e1e5053b19b20
Jusa Mankki <jusamankki@gmail.com>
2021-03-08 13:46:56 +0200

    8.7

--- viikko8/library/library-backend.js
+++ viikko8/library/library-backend.js
@@ -111,6 +111,10 @@ const typeDefs = gql`
       published: Int!
       genres: [String]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int
+    ): Author
   }
 `
 
@@ -146,6 +150,15 @@ const resolvers = {
         authors = authors.concat(newAuthorObject)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
