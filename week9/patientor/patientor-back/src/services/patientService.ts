498a6da3537cb57d828a880c0d455e07172caf59
Jusa Mankki <jusamankki@gmail.com>
2021-03-22 09:13:40 +0200

    9.19

--- week9/patientor/patientor-back/src/services/patientService.ts
+++ week9/patientor/patientor-back/src/services/patientService.ts
@@ -1,20 +1,24 @@
 
-import patientData from '../../data/patients.json';
+import patientData from '../../data/patients';
 
-import { PatientEntry, NonSensitivePatientEntry, NewPatientEntry } from '../types';
-import {v1 as uuid} from 'uuid'
+import { SinglePatientEntry, PatientEntry, NewPatientEntry, PublicPatient } from '../types';
+import { v1 as uuid } from 'uuid'
 
-const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
-  return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
-    id, name, dateOfBirth, gender, occupation, entries: []
+const getNonSensitiveEntries = (): PublicPatient[] => {
+  //console.log("PATIENT DATA", patientData)
+  return patientData.map(({ id, name, dateOfBirth, gender, occupation }: PublicPatient) => ({
+    id, name, dateOfBirth, gender, occupation
   }));
 };
 
-const findById = (id: string): PatientEntry | undefined => {
-  const entry = patientData.map(({ id, name, ssn, dateOfBirth, gender, occupation }) => ({
-    id, name, ssn, dateOfBirth, gender, occupation, entries: []
-  })).find(p => p.id === id);
-  return entry;
+const findById = (id: string): SinglePatientEntry | undefined => {
+  //console.log("PATIENT DATA", patientData)
+  const entry = patientData.map(({ id, name, ssn, dateOfBirth, gender, occupation, entries }: SinglePatientEntry) => ({
+    id, name, ssn, dateOfBirth, gender, occupation, entries
+  }));
+  const patient = entry.find((p: SinglePatientEntry) => p.id === id);
+  //console.log("PATIENT BY ID", patient)
+  return patient;
 }
 
 const addPatient = (entry: NewPatientEntry): PatientEntry => {
@@ -22,7 +26,7 @@ const addPatient = (entry: NewPatientEntry): PatientEntry => {
     id: uuid(),
     ...entry
   }
-  patientData.concat(newPatientEntry);
+  patientData.push(newPatientEntry);
   return newPatientEntry;
 }
 
--- week9/patientor/patientor-back/src/types.ts
+++ week9/patientor/patientor-back/src/types.ts
@@ -1,12 +1,60 @@
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
 export interface DiagnoseEntry {
   code: string,
   name: string,
   latin?: string
 }
-export interface Entry {
 
+//Health entries
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry['code']>;
 }
 
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string,
+    endDate: string
+  }
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string,
+    criteria: string
+  }
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+
+//Patient entries
 export interface PatientEntry {
   id: string;
   name: string;
@@ -14,16 +62,16 @@ export interface PatientEntry {
   ssn: string;
   gender: string;
   occupation: string;
-  entries: Entry[];
+  entries: Array<Entry>;
 }
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type SinglePatientEntry = UnionOmit<PatientEntry, ''>; //Why does this work?
 
 export type PublicPatient = Omit<PatientEntry, 'ssn' | 'entries'>;
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
 
 export type NewPatientEntry = Omit<PatientEntry, 'id'>;
-
-export enum Gender {
-  Male = 'male',
-  Female = 'female'
-}
